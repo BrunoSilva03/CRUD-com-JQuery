94a42f29ae4d92c06df488eaa9af005d74d69404
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-07-19 13:57:01 -0300

    jquery é potente mesmo hein

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 $(function() {
-    
+    var numTarefas = 0;
+
+    $('#buttonmais').click(function() {
+        abrirformulario();
+    });
+
+    $('#btncancelar').click(function() {
+        cancelar();
+    })
+
+
+    function abrirformulario() {
+        if(numTarefas == 0) {
+            $('#textoinicial').hide();
+            $('.soprohiddenfuncionar').fadeIn();
+        }
+    }
+
+    function cancelar() {
+        if(numTarefas == 0) {
+            limparInputs();
+            $('.soprohiddenfuncionar').hide();
+            $('#textoinicial').fadeIn();
+        }
+    }
+
+
+    function limparInputs() {
+        //limpa todos os campos inputs do formulário
+        $('#formulario input').val('');
+    }
 })
