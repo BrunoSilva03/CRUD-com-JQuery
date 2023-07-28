ed5e46ead2c90b80136dcf3338d9c5587c1813a1
BrunoSilva03 <bruno5ssilva02@gmail.com>
2023-07-28 15:56:12 -0300

    pegar id

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
-$(function() {
+$(function () {
     var numTarefas = 0;
     var contador = 0;
-    
 
-    $('#buttonmais').click(function() {
+
+    $('#buttonmais').click(function () {
         abrirformulario();
     });
 
-    $('#btnconfirmar').click(function() {
+    $('#btnconfirmar').click(function () {
         conferirCampos();
-        
+
 
     });
 
-    $('#btncancelar').click(function() {
+    $('#btncancelar').click(function () {
         cancelar();
     });
 
-    $('#formulario input').focusin(function() {
+    $('#formulario input').focusin(function () {
         $(this).css("background", "white");
         $(this).css("color", "black");
     });
 
     // digitar enter e ir pulando para o próximo campo de preenchimento campo
-    $('#inputnome').keyup(function() {
-        if(event.keyCode === 13) {
+    $('#inputnome').keyup(function () {
+        if (event.keyCode === 13) {
             event.preventDefault();
             $('#inputdata').focus();
         }
     });
 
-    $('#inputdata').keyup(function() {
-        if(event.keyCode === 13) {
+    $('#inputdata').keyup(function () {
+        if (event.keyCode === 13) {
             event.preventDefault();
             $('#inputhorario').focus();
         }
     });
 
-    $('#inputhorario').keyup(function() {
-        if(event.keyCode === 13) {
+    $('#inputhorario').keyup(function () {
+        if (event.keyCode === 13) {
             event.preventDefault();
             $('#btnconfirmar').click();
         }
     });
 
 
-    
+
 
     function abrirformulario() {
-        if(numTarefas == 0) {
+        if (numTarefas == 0) {
             $('#textoinicial').hide();
             $('.soprohiddenfuncionar').fadeIn();
             $('#buttonmais').hide();
@@ -61,15 +61,15 @@ $(function() {
     function conferirCampos() {
         var campotarefa = $('#inputnome');
 
-        if($('#inputnome').val() == '') {
+        if ($('#inputnome').val() == '') {
             alert('Todos os campos precisam ser preenchidos');
             campotarefa.css("background", "red");
 
-        } else if($('#inputdata').val() == '') {
+        } else if ($('#inputdata').val() == '') {
             alert('Todos os campos precisam ser preenchidos');
             $('#inputdata').css("background", "red");
 
-        } else if($('#inputhorario').val() == '') {
+        } else if ($('#inputhorario').val() == '') {
             alert('Todos os campos precisam ser preenchidos');
             $('#inputhorario').css("background", "red");
         } else {
@@ -114,21 +114,21 @@ $(function() {
         </div>`
         */
 
-        
+
         $('.soprohiddenfuncionar').hide();
         limparInputs();
         $('#buttonmais').fadeIn();
-       //resultado.innerHTML += novaTarefa;
+        //resultado.innerHTML += novaTarefa;
         $('#areaLista').append(`
         <div class="tarefa" id="${contador}">
            <div class="icone-tarefa">
-               <i id="icone" class="mdi mdi-circle-outline" onclick="marcarTarefa(${contador})"></i>
+               <i id="icone_${contador}" class="mdi mdi-circle-outline" onclick="marcarTarefa(${contador})"></i>
            </div>
 
            <div class="conteudo-tarefa">
                <p id="nometarefa_${contador}">${nomeTarefa}</p>
                <p id="datatarefa_${contador}">${dataTarefa}</p>
-               <p id="horariotarefa_${contador}${horarioTarefa}">15:30</p>
+               <p id="horariotarefa_${contador}">${horarioTarefa}</p>
 
                <div class="areabotoestarefa">
                    <p>
@@ -164,7 +164,7 @@ $(function() {
         alert('oi');
     }
 
-    $('#btnupdatetarefa').click(function() {
+    $('#btnupdatetarefa').click(function () {
         alert('Você clicou no botão de update');
     })
 
@@ -173,36 +173,33 @@ $(function() {
         alert('Funcionou!');
     });
 
-   $('.tarefa').click(function() {
+    $('.tarefa').click(function () {
         alert('Você clicou na tarefa!!!!!')
-   })
+    })
 
-   
-   //O unbind serve para não realizar o evento em forma de fila várias vezes, pq eventualmente o usuário vai clicar na área do 
-   //#areaLista várias vezes, e #btnupdatetarefa não sobrescreveria e sim seria enfileirado. 
-    $('#areaLista').hover(function() {
+
+    //O unbind serve para não realizar o evento em forma de fila várias vezes, pq eventualmente o usuário vai clicar na área do 
+    //#areaLista várias vezes, e #btnupdatetarefa não sobrescreveria e sim seria enfileirado. 
+    $('#areaLista').hover(function () {
+
+
+        //TAREFA FEITA E NÃO FEITA MARCADOR
+        
+        $(`#icone_${idTarefa}`).unbind("click").on('click', function marcarTarefa(idTarefa) {
+            if ($(this).hasClass('mdi-circle-outline')) {
+                $(this).removeClass('mdi-circle-outline').addClass('mdi-check-circle');
+                $('.tarefa').removeClass('tarefa').addClass('tarefa-feito');
+            } else {
+                $(this).removeClass('mdi-check-circle').addClass('mdi-circle-outline');
+                $('.tarefa-feito').removeClass('tarefa-feito').addClass('tarefa');
+            }
+        })
 
         
-                //TAREFA FEITA E NÃO FEITA MARCADOR
-                $('#icone').unbind("click").on('click', function() {
-                    if($(this).hasClass('mdi-circle-outline')) {
-                        $(this).removeClass('mdi-circle-outline').addClass('mdi-check-circle');
-                        $('.tarefa').removeClass('tarefa').addClass('tarefa-feito');
-                    } else {
-                            $(this).removeClass('mdi-check-circle').addClass('mdi-circle-outline');
-                            $('.tarefa-feito').removeClass('tarefa-feito').addClass('tarefa');
-                    }
-                })
-           
-        /*
-        $('.tarefa').unbind("click").click(function marcarTarefa(idTarefa) {
-            $(this).removeClass('tarefa').addClass('tarefa-feito');
-        });
-        */
 
-        $('.tarefa').hover(function() {
-            $('.icone-tarefa').hover(function() {
-                $('.icone').unbind("click").on('click', function() {
+        $('.tarefa').hover(function () {
+            $('.icone-tarefa').hover(function () {
+                $('.icone').unbind("click").on('click', function () {
                     $(this).removeClass('mdi-circle-outline').addClass('mdi-check-circle');
                 })
             })
@@ -210,15 +207,15 @@ $(function() {
 
         $('.tarefa-feito').unbind("dblclick").dblclick(function marcarTarefa(idTarefa) {
             $(this).removeClass('tarefa-feito').addClass('tarefa');
-            
+
         })
 
-        $('mdi').unbind("dblclick").on('click', function() {
+        $('mdi').unbind("dblclick").on('click', function () {
             $(this).removeClass('mdi-circle-outline').addClass('mdi-check-circle');
-            
+
         });
 
-        $('#btnupdatetarefa').unbind("click").on('click', function() {
+        $('#btnupdatetarefa').unbind("click").on('click', function () {
             alert('Tá clicando no update tarefa');
         });
 
@@ -226,17 +223,17 @@ $(function() {
             alert('Tá clicando no botão de excluir tarefa');
         });
     })
-    
 
-    
+
+
 
 
     function updateTarefa(id) {
-      
+
     }
 
     function cancelar() {
-        if(numTarefas == 0) {
+        if (numTarefas == 0) {
             limparInputs();
             $('.soprohiddenfuncionar').hide();
             $('#textoinicial').fadeIn();
@@ -258,7 +255,7 @@ $(function() {
 
 
     function confereFooter() {
-        if(numTarefas >= 2) {
+        if (numTarefas >= 2) {
             $('footer').css("background", "#4ae4279d");
         } else {
             $('footer').css("background", "#39e612");
